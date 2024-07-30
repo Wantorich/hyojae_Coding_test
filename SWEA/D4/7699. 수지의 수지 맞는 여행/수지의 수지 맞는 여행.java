ed72c54db94c1797a4b6d00403bd74f153d6a016
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-07-30 16:10:27 +0900

    [D4] Title: 수지의 수지 맞는 여행, Time: 1,988 ms, Memory: 21,520 KB -BaekjoonHub

--- SWEA/D4/7699. 수지의 수지 맞는 여행/수지의 수지 맞는 여행.java
+++ SWEA/D4/7699. 수지의 수지 맞는 여행/수지의 수지 맞는 여행.java
@@ -1,7 +1,8 @@
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -9,7 +10,6 @@ public class Solution {
     static int [] scores, cals;
     static int result, R, C;
     static char [][] grid;
-    static boolean [][] v;
     static int [] alpha;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -21,7 +21,6 @@ public class Solution {
             R = Integer.parseInt(line[0]);
             C = Integer.parseInt(line[1]);
             grid = new char[R+1][C+1];
-            v = new boolean[R+1][C+1];
             result = Integer.MIN_VALUE;
             
             for (int i = 1; i <= R; i++) {
@@ -35,7 +34,10 @@ public class Solution {
             alpha[grid[1][1] - 'A'] = 1;
             dfs(1, 1, 1);
             
-            System.out.printf("#%d %d\n", t, result);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            String answer = String.format("#%d %d\n", t, result);
+            bw.write(answer);
+            bw.flush();
         }
     }
 
@@ -43,7 +45,6 @@ public class Solution {
     static int [] dc = {0, 1, 0, -1};
     
     static void dfs(int r, int c, int cnt) {
-    	
     	for (int i = 0; i < dr.length; i++) {
     		int nr = r + dr[i];
     		int nc = c + dc[i];

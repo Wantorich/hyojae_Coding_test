5344a3d0c0f0cba39421ba0bc0ec446206b50b92
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-08-22 20:08:58 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 1000 ms, Memory: 105388 KB -BaekjoonHub

---
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Main {
+	static int N, M, R, grid[][];
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		R = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+
+		grid = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++)
+				grid[i][j] = sc.nextInt();
+		}
+
+		int wholeRotate = Math.min(N, M) / 2;
+		int r = 0, c = 0;
+		for (int t = 0; t < wholeRotate; t++) {
+			// 전체 회전
+			// 시작점은 0,0 -> 1,1 / r+1, c+1
+
+			int colEnd = M - t - 1;
+			int rowEnd = N - t - 1;
+			
+			// colEnd - t + 1 이게 colSize
+			// rowEnd - t + 1 이게 rowSize
+			int rotateSize = 2 * (colEnd-t+1 + rowEnd-t+1) - 4;
+			// 껍질의 길이구하기 그리고 R % 길이 만큼만 안에서 돌리기
+			for (int k = 0; k < R % rotateSize; k++) {
+				int initialVal = grid[r][c];
+
+				// 오른쪽으로 돌면서 다음값을 현재값에 저장함
+				for (int j = t; j < colEnd; j++) {
+					grid[r][j] = grid[r][j + 1];
+				}
+
+				// 아래방향
+				for (int i = t; i < rowEnd; i++) {
+					grid[i][colEnd] = grid[i + 1][colEnd];
+				}
+
+				// 왼쪽 방향
+				for (int j = colEnd; j > t; j--) {
+					grid[rowEnd][j] = grid[rowEnd][j - 1];
+				}
+
+				// 위쪽 방향
+				for (int i = rowEnd; i > t; i--) {
+					grid[i][t] = grid[i - 1][t];
+				}
+
+				// 처음 저장한 값 넣어줌
+				grid[r + 1][c] = initialVal;
+			}
+
+			// 두번째 회전은 N-2, M-2 격자임
+			r++;
+			c++;
+		}
+
+		for (int[] row : grid) {
+			for (int val : row)
+				sb.append(val).append(" ");
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString());
+		sc.close();
+	}
+}

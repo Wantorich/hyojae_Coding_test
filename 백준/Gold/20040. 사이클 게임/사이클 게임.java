107ef98234964f95b240440fe913c52be0897c11
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-09-04 11:31:46 +0900

    [Gold IV] Title: 사이클 게임, Time: 636 ms, Memory: 272236 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M, parents[];
+	static List<int[]> edgeList;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        StringTokenizer st;
+        
+        String input[] = br.readLine().split(" ");
+        
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        
+        edgeList = new ArrayList(M);
+        
+        for (int i = 0; i < M; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	int from = Integer.parseInt(st.nextToken());
+        	int to = Integer.parseInt(st.nextToken());
+        	edgeList.add(new int[] {from, to});
+        }
+        
+        int result = 0;
+        int idx = 0;
+        makeSet();
+        for (int [] edge : edgeList) {
+        	idx++;
+        	if (!union(edge[0], edge[1])) {
+        		result = idx;
+        		break;
+        	}
+        }
+        
+        System.out.println(result);
+        
+        sc.close();
+    }
+    
+    static void makeSet() {
+    	parents = new int[N];
+    	for (int i = 0; i < N; i++) parents[i] = i;
+    }
+    
+    static int find(int v) {
+    	if (parents[v] == v) return v;
+    	return parents[v] = find(parents[v]);
+    }
+    
+    static boolean union(int i, int j) {
+    	int pi = find(i);
+    	int pj = find(j);
+    	if (pi == pj) return false;
+    	parents[pi] = pj;
+    	return true;
+    }
+}

92797d6201d6f588eed010be74e9b1fafb25be83
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-09-05 09:35:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 88 ms, Memory: 12848 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -8,7 +8,9 @@ public class Main {
         int tc = sc.nextInt();
         for (int t = 1; t <= tc; t++) {
         	X = sc.nextInt();
-        	dp(X);
+        	memo = new int[12];
+        	dp2(X);
+        	System.out.println(memo[X]);
         }
         
         sc.close();
@@ -22,5 +24,23 @@ public class Main {
 		}
 		System.out.println(memo[x]);
 	}
-
+	
+	static int [] memo;
+	
+	static int dp2(int x) {
+		if (x < 0) return 0;
+		else if (x <= 1) return memo[x] = 1;
+		if (memo[x] != 0) return memo[x];
+		
+		return memo[x] = Math.max(memo[x], dp2(x-3) + dp2(x-2) + dp2(x-1));
+	}
 }
+
+/*
+1 -> (1) -> 1
+2 -> (1, 1), (2) -> 2
+3 -> (1, 1, 1), (1, 2), (2, 1), (3) -> 4
+4 -> (1, 1, 1, 1), (1, 2, 1), (2, 1, 1), (1, 1, 2), (1, 3), (3, 1), (2, 2) -> 7
+5 -> (1, 1, 1, 1, 1), (2, 1, 1, 1), (1, 2, 1, 1), (1, 1, 2, 1), (1, 1, 1, 2), (1, 1, 3)
+(3, 1, 1), (1, 3, 1), (1, 2, 2), (2, 1, 2), (2, 2, 1), (3, 2), (2, 3) -> 13
+*/

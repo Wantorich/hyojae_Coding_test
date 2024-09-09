e29482e4244d3d5eb4e23800501623f6d77599be
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-09-09 19:52:30 +0900

    [Silver II] Title: 최대 페이지 수, Time: 92 ms, Memory: 12916 KB -BaekjoonHub

--- 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.java
+++ 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.java
@@ -18,7 +18,8 @@ public class Main {
         	pages[i] = sc.nextInt();
         }
         
-        System.out.println(recur(M, N));
+//        System.out.println(recur(M, N));
+        System.out.println(dp());
 //        for (int [] row : memo) System.out.println(Arrays.toString(row));
         sc.close();
     }
@@ -28,6 +29,19 @@ public class Main {
 		if (k == 0) return 0;
 		if (memo[k][day] != 0) return memo[k][day];
 		return memo[k][day] = Math.max(recur(k-1, day-days[k]) + pages[k], recur(k-1, day));
+	}
+	
+	private static int dp() {
+		int dpTable[][] = new int[M+1][N+1];
+		for (int i = 1; i <= M; i++) {
+			for (int j = 1; j <= N; j++) {
+				dpTable[i][j] = dpTable[i-1][j];
+				if (j - days[i] >= 0) 
+					dpTable[i][j] = Math.max(dpTable[i-1][j], dpTable[i-1][j - days[i]] + pages[i]);
+			}
+		}
+//		for (int [] row : dpTable) System.out.println(Arrays.toString(row));
+		return dpTable[M][N];
 	}
 }
 

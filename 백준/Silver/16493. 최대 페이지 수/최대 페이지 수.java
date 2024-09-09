16e14c18f72ac5733146d23725b69b39bf29bc34
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-09-09 19:44:24 +0900

    [Silver II] Title: 최대 페이지 수, Time: 92 ms, Memory: 13020 KB -BaekjoonHub

--- 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.java
+++ 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.java
@@ -2,38 +2,32 @@ import java.util.*;
 
 public class Main {
 	static int days[], pages[], N, M, result = Integer.MIN_VALUE;
-	static int []memo;
+	static int [][]memo;
 	
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
         
-        days = new int[M];
-        pages = new int[M];
-        memo = new int[M+1];
+        days = new int[M+1];
+        pages = new int[M+1];
+        memo = new int[M+1][N+1];
         
-        for (int i = 0; i < M; i++) {
+        for (int i = 1; i <= M; i++) {
         	days[i] = sc.nextInt();
         	pages[i] = sc.nextInt();
         }
-        recur(0, 0, 0);
-        System.out.println(result);
         
-        
+        System.out.println(recur(M, N));
+//        for (int [] row : memo) System.out.println(Arrays.toString(row));
         sc.close();
     }
 
-	private static void recur(int k, int day, int cnt) {
-		if (day > N) return;
-		
-		if (k >= M) {
-			result = Math.max(result, cnt);
-			return;
-		}
-		
-		recur(k+1, day+days[k], cnt + pages[k]);
-		recur(k+1, day, cnt);
+	private static int recur(int k, int day) {
+		if (day < 0) return Integer.MIN_VALUE;
+		if (k == 0) return 0;
+		if (memo[k][day] != 0) return memo[k][day];
+		return memo[k][day] = Math.max(recur(k-1, day-days[k]) + pages[k], recur(k-1, day));
 	}
 }
 

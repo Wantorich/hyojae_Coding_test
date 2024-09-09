f59f114d27ee11443dddc7a501186d069835abbd
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-09-09 16:20:14 +0900

    [D3] Title: 최장 공통 부분 수열, Time: 258 ms, Memory: 51,604 KB -BaekjoonHub

--- SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.java
+++ SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.java
@@ -12,7 +12,8 @@ public class Solution {
 			charArr1 = sc.next().toCharArray();
 			charArr2 = sc.next().toCharArray();
 			memo = new int[charArr1.length][charArr2.length];
-			int result = recur(charArr1.length-1, charArr2.length-1);
+//			int result = recur(charArr1.length-1, charArr2.length-1);
+			int result = dp();
 			System.out.printf("#%d %d\n", t, result);
 		}
 		sc.close();
@@ -24,5 +25,18 @@ public class Solution {
 		
 		if (charArr1[m] == charArr2[n]) return memo[m][n] = recur(m-1, n-1) + 1;
 		else return memo[m][n] = Math.max(recur(m-1, n), recur(m, n-1));
+	}
+	
+	private static int dp() {
+		int dp[][] = new int[charArr1.length+1][charArr2.length+1];
+		
+		for (int i = 1; i <= charArr1.length; i++) {
+			for (int j = 1; j <= charArr2.length; j++) {
+				if (charArr1[i-1] == charArr2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
+				else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+			}
+		}
+//		for (int [] row : dp) System.out.println(Arrays.toString(row));
+		return dp[charArr1.length][charArr2.length];
 	}
 }

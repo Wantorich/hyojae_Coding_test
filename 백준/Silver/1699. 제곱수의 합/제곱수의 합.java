bed00b0b43353039afa68554dfdea34068acf47a
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-09-13 15:22:34 +0900

    [Silver II] Title: 제곱수의 합, Time: 128 ms, Memory: 13712 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -7,9 +7,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         memo = new int[N+1];
-        System.out.println(recur(N));
-//        System.out.println(dp(N));
-//        System.out.println(Arrays.toString(memo));
+//        System.out.println(recur(N));
+        System.out.println(dp(N));
         sc.close();
     }
 
@@ -26,8 +25,6 @@ public class Main {
 				min = Math.min(min, recur(n - sqrt * sqrt) + 1);
 				sqrt--;
 			}
-//			int next = n - sqrt * sqrt;
-//			System.out.println(next);
 			return memo[n] = min;
 		}
 	}
@@ -35,23 +32,18 @@ public class Main {
 	private static int dp(int n) {
 		int [] table = new int[n+1];
 		Arrays.fill(table, Integer.MAX_VALUE);
-//		table[1] = 1;
-		int sqrt = 0;
-		List<Integer> sqrtList = new ArrayList<Integer>();
 		for (int num = 1; num <= n; num++) {
-			sqrt = (int) Math.sqrt(num);
+			int sqrt = (int) Math.sqrt(num);
 			if (sqrt * sqrt == num) {
 				table[num] = 1;
-				sqrtList.add(num);
-				for (int i = 0; i < sqrtList.size(); i++) {
-					int tmp = sqrtList.get(i);
-					if (tmp + num <= n) table[tmp+num] = 2;
-				}
 			}
 			else {
-				int diff = num - sqrt * sqrt;
-//				System.out.println(diff);
-				table[num] = Math.min(table[num], table[diff] + 1); 
+				int min = Integer.MAX_VALUE;
+				while (sqrt >= 1) {
+					min = Math.min(min, table[num - sqrt * sqrt] + 1);
+					sqrt--;
+				}
+				table[num] = min;
 			}
 		}
 //		System.out.println(Arrays.toString(table));

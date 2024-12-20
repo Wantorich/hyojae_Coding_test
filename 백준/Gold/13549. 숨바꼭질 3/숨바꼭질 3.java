6c9c6c1d9492fe4853a8bc40531d5cd9475805ed
ChoiHyoJae <121792083+Wantorich@users.noreply.github.com>
2024-12-21 01:04:07 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 108 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,73 +1,45 @@
 import java.util.*;
 
 public class Main {
-	static int N, K, answer = Integer.MAX_VALUE, time[];
-	static int INF = Integer.MAX_VALUE;
-	static int MAX_NUM = 100000;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt(); K = sc.nextInt();
-		boolean[] visit = new boolean[K+1];
-		
-		if (N >= K) {
-			answer = N - K;
-			System.out.println(answer);
-			return;
-		}
-		
-		Queue<int[]> q = new LinkedList<>();
-		Queue<int[]> temp = new LinkedList<>();
-		q.offer(new int[] {N, 0});
-		
-		O:while (!q.isEmpty()) {
-			// 뽑아서 전부 2배 업 시킴
-			while (!q.isEmpty()) {
-				int[] info = q.poll();
-				int num = info[0];
-				while (num <= K && !visit[num]) {
-					visit[num] = true;
-					temp.offer(new int[] {num, info[1]});
-					if (num == K) {
-						answer = info[1];
-						break O;
-					}
-					num *= 2;
-				}
-				
-				if (num > K) {
-					answer = Math.min(answer, info[1] + num - K);
-//					System.out.println("answer : " + answer);
-				}
-			}
-			
-			while (!temp.isEmpty()) {
-				int[] info = temp.poll();
-				int num = info[0];
-				
-				int left = num-1;
-				int right = num+1;
-				
-				if (left == K || right == K) {
-					answer = Math.min(answer, info[1] + 1);
-					break O;
-				}
-				
-				if (left >= 0 && !visit[left]) {
-//					visit[left] = true;
-					q.offer(new int[] {left, info[1]+1});
-				}
-				
-				if (right <= K && !visit[right]) {
-//					visit[right] = true;
-					q.offer(new int[] {right, info[1]+1});
-				}
-			}
-		}
-		
-		System.out.println(answer);
-//		System.out.println(Arrays.toString(visit));
-		sc.close();
-	}
+    static int N, K, answer;
+    static int MAX_NUM = 100000;
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        boolean[] visit = new boolean[MAX_NUM + 1];
+
+        if (N >= K) {
+            System.out.println(N - K);
+            return;
+        }
+
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {N, 0});
+
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            int position = current[0];
+            int time = current[1];
+
+            if (position == K) {
+                System.out.println(time);
+                return;
+            }
+
+            if (position * 2 <= MAX_NUM && !visit[position * 2]) {
+                visit[position * 2] = true;
+                q.addFirst(new int[] {position * 2, time});
+            }
+            if (position - 1 >= 0 && !visit[position - 1]) {
+                visit[position - 1] = true;
+                q.offer(new int[] {position - 1, time + 1});
+            }
+            if (position + 1 <= MAX_NUM && !visit[position + 1]) {
+                visit[position + 1] = true;
+                q.offer(new int[] {position + 1, time + 1});
+            }
+        }
+    }
 }

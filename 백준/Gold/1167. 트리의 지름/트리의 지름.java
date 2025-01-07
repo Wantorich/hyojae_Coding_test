3cb1142b980204ddb59c5de8d175b426fb5bdb94
ChoiHyoJae <121792083+Wantorich@users.noreply.github.com>
2025-01-07 14:33:54 +0900

    [Gold II] Title: 트리의 지름, Time: 696 ms, Memory: 97452 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static boolean[] visit;
 	static int maxLen, maxVertex;
 	static List<int[]>[] adjList;
 
@@ -27,18 +26,16 @@ public class Main {
 			}
 		}
 		
-		visit = new boolean[N+1];
-		Arrays.fill(visit, false);
-		visit[1] = true;
-		dfs(1, 0);
-		visit[1] = false;
-		
-		visit[maxVertex] = true;
-		dfs(maxVertex, 0);
+		// 임의의 노드에서 가장 먼 곳 찾음
+		dfs(1, 0, new boolean[N+1]);
+		// 가장 긴 지름 찾음
+		dfs(maxVertex, 0, new boolean[N+1]);
 		System.out.println(maxLen);
 	}
 
-	private static void dfs(int v, int dis) {
+	private static void dfs(int v, int dis, boolean[] visit) {
+		visit[v] = true;
+		
 		if (dis > maxLen) {
 			maxLen = dis;
 			maxVertex = v;
@@ -47,9 +44,7 @@ public class Main {
 		for (int[] connect : adjList[v]) {
 			int next = connect[0];
 			if (visit[next]) continue;
-			visit[next] = true;
-			dfs(next, dis + connect[1]);
-			visit[next] = false;
+			dfs(next, dis + connect[1], visit);
 		}
 	}
 }

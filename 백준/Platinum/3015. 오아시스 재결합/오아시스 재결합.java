43d7e6e3e89b9d6da53c86040e5b072b6c2d3219
ChoiHyoJae <121792083+Wantorich@users.noreply.github.com>
2025-01-10 15:53:48 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 396 ms, Memory: 62940 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
@@ -8,34 +8,22 @@ public class Main {
     	int height;
     	ArrayDeque<int[]> st = new ArrayDeque<>();
     	long answer = 0;
-    	Map<Integer, Integer> same = new HashMap<>();
     	
     	for (int i = 1; i <= N; i++) {
     		height = Integer.parseInt(br.readLine());
-    		
-    		if (!st.isEmpty() && st.peek()[0] > height) {
-				answer++;
-				st.push(new int[] {height, i});
-				continue;
-    		}
+    		int sameCount = 1;
     		
     		while (!st.isEmpty() && st.peek()[0] <= height) {
-    			answer++;
-				// peek < height
-				if (same.getOrDefault(st.peek()[0], 0) > 0) {
-					answer += same.get(st.peek()[0]);
-					if (st.peek()[0] < height)
-						same.put(st.peek()[0], 0);
-				}
-				
-				if (st.peek()[0] == height) {
-					same.put(height, same.getOrDefault(height, 0) + 1);
-				} 
-    			st.pop();
-    		}	
+    			answer += st.peek()[1];
+    			
+    			if (st.peek()[0] == height) 
+    				sameCount += st.pop()[1];
+    			else
+    				st.pop();
+    		}
     		
     		if (!st.isEmpty()) answer++;
-			st.push(new int[] {height, i});
+    		st.push(new int[] {height, sameCount});
     	}
     	System.out.println(answer);
     }

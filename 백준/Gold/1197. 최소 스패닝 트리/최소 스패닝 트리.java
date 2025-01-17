680bf7675105e0aacebff116d6e9898da17073cf
ChoiHyoJae <121792083+Wantorich@users.noreply.github.com>
2025-01-17 21:59:56 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 492 ms, Memory: 49016 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,45 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	static int[] parent;
+	
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
     	int V = Integer.parseInt(st.nextToken());
     	int E = Integer.parseInt(st.nextToken());
-
-    	List<int[]>[] adjList = new ArrayList[V+1];
-    	for (int i = 1; i <= V; i++)
-    		adjList[i] = new ArrayList<>();
-
+    	parent = new int[V+1];
+    	Arrays.fill(parent, -1);
+    	PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(a[2], b[2]));
+    	
     	int from, to, weight;
     	for (int i = 0; i < E; i++) {
     		st = new StringTokenizer(br.readLine(), " ");
     		from = Integer.parseInt(st.nextToken());
     		to = Integer.parseInt(st.nextToken());
     		weight = Integer.parseInt(st.nextToken());
-    		adjList[from].add(new int[] {to, weight});
-    		adjList[to].add(new int[] {from, weight});
+    		pq.offer(new int[] {from, to, weight});
     	}
     		
     	long result = 0;
-    	boolean[] visit = new boolean[V+1];
-    	
-    	PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(a[1], b[1]));
-    	pq.offer(new int[] {1, 0});
-    	
     	while (!pq.isEmpty()) {
     		int[] edge = pq.poll();
-    		
-    		if (visit[edge[0]]) continue;
-    		visit[edge[0]] = true;
-    		result += edge[1];
-    		
-    		for (int[] connect : adjList[edge[0]]) {
-    			if (visit[connect[0]]) continue;
-    			pq.offer(new int[] {connect[0], connect[1]});
-    		}
+    		if (!union(edge[0], edge[1])) 
+    			continue;
+    		result += edge[2];
     	}
-    	
     	System.out.println(result);
+    }
+    
+    static int find(int a) {
+    	if (parent[a] == -1) return a;
+    	return parent[a] = find(parent[a]);
+    }
+    
+    static boolean union(int a, int b) {
+    	int rootA = find(a);
+    	int rootB = find(b);
+    	if (rootA == rootB) return false;
+    	parent[rootA] = rootB;
+    	return true;
     }
 }

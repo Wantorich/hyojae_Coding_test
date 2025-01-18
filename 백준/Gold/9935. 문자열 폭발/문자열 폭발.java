fecaf2b4044425bfb360d2ee8a03ec421d14b740
ChoiHyoJae <121792083+Wantorich@users.noreply.github.com>
2025-01-18 12:24:19 +0900

    [Gold IV] Title: 문자열 폭발, Time: 572 ms, Memory: 57928 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,37 +1,35 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-    	Scanner sc = new Scanner(System.in);
-    	String line = sc.nextLine();
-    	String bomb = sc.nextLine();
-    	Deque<Character> stack = new ArrayDeque<>();
-    	Deque<Character> temp = new ArrayDeque<>();
-    	
-    	for (int i = 0; i < line.length(); i++) {
-    		char c = line.charAt(i);
-    		stack.push(c);
-    		if (bomb.indexOf(c + "") == bomb.length()-1) {
-    			int index = bomb.length() - 1;
-    			while (!stack.isEmpty() && index >= 0 &&
-    					(stack.peek() == bomb.charAt(index--))) {
-    				temp.push(stack.pop());
-    			}
-    			if (temp.size() == bomb.length()) 
-    				temp.clear();
-    			else {
-    				while (!temp.isEmpty())
-    					stack.push(temp.pop());
-    			}
-    		}
-    	}
-    	
-    	StringBuilder sb = new StringBuilder();
-    	while (!stack.isEmpty())
-    		sb.append(stack.pollLast());
-    	String answer = sb.length() == 0 ? "FRULA" : sb.toString();
-    	System.out.println(answer);
-    	sc.close();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 입력 받기
+        String input = sc.nextLine(); // 원본 문자열
+        String bomb = sc.nextLine(); // 폭발 문자열
+        
+        int bombLength = bomb.length();
+        char lastChar = bomb.charAt(bombLength - 1); // 폭발 문자열의 마지막 문자
+        
+        // 스택 역할을 하는 StringBuilder
+        StringBuilder stack = new StringBuilder();
+        
+        for (char c : input.toCharArray()) {
+            stack.append(c); // 현재 문자를 스택에 추가
+            
+            // 스택의 끝부분이 폭발 문자열과 일치하는지 확인
+            if (stack.length() >= bombLength && stack.charAt(stack.length() - 1) == lastChar) {
+                if (stack.substring(stack.length() - bombLength).equals(bomb)) {
+                    stack.delete(stack.length() - bombLength, stack.length()); // 폭발 문자열 제거
+                }
+            }
+        }
+        
+        // 결과 출력
+        if (stack.length() == 0) {
+            System.out.println("FRULA");
+        } else {
+            System.out.println(stack.toString());
+        }
     }
 }

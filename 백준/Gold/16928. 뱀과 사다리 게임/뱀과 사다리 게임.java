0264a19b099a8da4be458811d416ed5bc2cbec68
ChoiHyoJae <121792083+Wantorich@users.noreply.github.com>
2025-01-20 21:19:22 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 556 ms, Memory: 279588 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -5,8 +5,8 @@ public class Main {
     	Scanner sc = new Scanner(System.in);
     	int N = sc.nextInt();
     	int M = sc.nextInt();
-    	int[] attemps = new int[101];
-    	Arrays.fill(attemps, -1);
+    	boolean[] visit = new boolean[101];
+    	
     	Map<Integer, Integer> portal = new HashMap<>();
     	for (int i = 0; i < N+M; i++) {
     		int in = sc.nextInt();
@@ -22,21 +22,22 @@ public class Main {
 			int curr = info[0];
 			int attemp = info[1];
 			
-			if (portal.get(curr) != null) {
-				attemps[portal.get(curr)] = attemp;
-				queue.offerFirst(new int[] {portal.get(curr), attemp});
-				continue;
-			}
-			
-			attemps[curr] = attemp;
 			if (curr == 100) {
 				System.out.println(attemp);
 				return;
 			}
 			
+			if (portal.containsKey(curr)) {
+				visit[curr] = true;
+				queue.offerFirst(new int[] {portal.get(curr), attemp});
+				continue;
+			}
+			
+			visit[curr] = true;
+			
 			for (int i = 1; i <= 6; i++) {
 				int next = curr + i;
-				if (next <= 100 && attemps[next] == -1)
+				if (next <= 100 && !visit[next])
 					queue.offer(new int[] {next, attemp+1});
 			}
     	}

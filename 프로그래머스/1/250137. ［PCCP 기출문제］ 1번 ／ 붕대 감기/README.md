e79779ff17210ce78dd2dc774d04f3ae30bc130a
ChoiHyoJae <121792083+Wantorich@users.noreply.github.com>
2025-08-06 12:51:26 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.10 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/README.md
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCP 기출문제] 1번 / 붕대 감기 - 250137 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250137?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250137) 
 
 ### 성능 요약
 
-메모리: 80.5 MB, 시간: 3.04 ms
+메모리: 87.5 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 06일 11:57:43
+2025년 08월 06일 12:51:23
 
 ### 문제 설명
 
--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,26 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int end = attacks[attacks.length-1][0];
-        int idx = 0;
-        int MAX_HEALTH = health;
-        int healCnt = 0;
-        for (int i = 1; i <= end; i++) {
-            if (attacks[idx][0] == i) {
-                health -= attacks[idx++][1];
-                if (health <= 0) return -1;
-                healCnt = 0;
-                continue;
+        int cnt = 0, currTime = 1, life = health;
+        for (int[] attack : attacks) {
+            while (currTime++ < attack[0]) {
+                life = Math.min(health, life + bandage[1]);
+                if (++cnt == bandage[0]) {
+                    life = Math.min(health, life + bandage[2]);
+                    cnt = 0;
+                }
             }
-            
-            health = Math.min(MAX_HEALTH, health + bandage[1]);
-            healCnt++;
-            if (healCnt == bandage[0]) {
-                health = Math.min(MAX_HEALTH, health + bandage[2]);
-                healCnt = 0;
+            life -= attack[1];
+            cnt = 0;
+            if (life <= 0) {
+                return -1;
             }
         }
-        return health;
+        
+        return life;
     }
 }

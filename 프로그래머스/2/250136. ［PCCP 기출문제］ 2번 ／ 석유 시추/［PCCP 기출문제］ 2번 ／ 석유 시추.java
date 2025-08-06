3abc5fd2897775b9c9150020a2e9e25c98795788
ChoiHyoJae <121792083+Wantorich@users.noreply.github.com>
2025-08-06 13:09:35 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 83.92 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,64 +1,66 @@
 import java.util.*;
 
 class Solution {
-    static boolean visit[][];
-    static int MAX_ROW, MAX_COL, energyCnt;
+    int[] dr = {-1, 0, 1, 0};
+    int[] dc = {0, 1, 0, -1};
     
     public int solution(int[][] land) {
-        MAX_ROW = land.length;
-        MAX_COL = land[0].length;
-        visit = new boolean[MAX_ROW][MAX_COL];
-        int [] energySum = new int[MAX_COL];
-        for (int i = 0; i < MAX_ROW; i++) {
-            for (int j = 0; j < MAX_COL; j++) {
-                if (land[i][j] == 0 || visit[i][j]) continue;
-                // Set<Integer> colSet = new HashSet<>();
-                boolean [] visitCol = new boolean[MAX_COL];
-                energyCnt = 0;
-                // colSet.add(j);
-                bfs(i, j, visitCol, land);
-                for (int k = 0; k < MAX_COL; k++) {
-                    if (visitCol[k]) energySum[k] += energyCnt;
+        int answer = 0;
+        int N = land.length;
+        int M = land[0].length;
+        boolean[][] v = new boolean[N][M];
+        int[] mounts = new int[M];
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (!v[i][j] && land[i][j] == 1) {
+                    List<Integer> info = calculate(i, j, land, v);
+                    int cnt = info.get(info.size()-1);
+                    for (int k = 0; k < info.size() - 1; k++) {
+                        mounts[info.get(k)] += cnt;
+                    }
                 }
-                // for (int col : colSet)
-                //     energySum[col] += energyCnt;
             }
         }
         
-        int answer = Arrays.stream(energySum).max().getAsInt();
-        return answer;
+        return Arrays.stream(mounts).max().getAsInt();
     }
     
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
-    
-    static void bfs(int r, int c, boolean[] visitCol, int[][] land) {
+    List<Integer> calculate(int r, int c, int[][] land, boolean[][] v) {
+        Set<Integer> result = new HashSet<>();
+        result.add(c);
+        v[r][c] = true;
         Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{r, c});
-        visit[r][c] = true;
-        visitCol[c] = true;
-        energyCnt++;
-        // set.add(c);
+        int cnt = 1;
         
+        
+        int nr, nc;
         while (!q.isEmpty()) {
-            int [] p = q.poll();
+            int[] curr = q.poll();
             
             for (int i = 0; i < dr.length; i++) {
-                int nr = p[0] + dr[i];
-                int nc = p[1] + dc[i];
-
-                if (nr < 0 || nr >= MAX_ROW || nc < 0 || nc >= MAX_COL || visit[nr][nc] || land[nr][nc] == 0) 
+                nr = curr[0] + dr[i];
+                nc = curr[1] + dc[i];
+                
+                if (nr < 0 || nr >= land.length || nc < 0 || nc >= land[0].length 
+                   || v[nr][nc] || land[nr][nc] == 0) {
                     continue;
-
-                // if (c != nc) 
-                //     set.add(nc);
-                // dfs(nr, nc, visitCol, land);
-                visit[nr][nc] = true;
-                visitCol[nc] = true;
-                energyCnt++;
+                }
+                
+                v[nr][nc] = true;
                 q.offer(new int[]{nr, nc});
-             }
+                result.add(nc);
+                cnt++;
+            }
         }
         
+        List<Integer> list = new ArrayList(result);
+        list.add(cnt);
+        return list;
     }
 }
+
+/*
+dfs를 하면서 열만 저장하면 된다
+*/

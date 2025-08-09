d6234a82c79e6085748c09a5c2477e06b81c4308
ChoiHyoJae <121792083+Wantorich@users.noreply.github.com>
2025-08-09 10:45:40 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 42.42 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,26 +1,39 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int answer = 0;
-        long left = 1, right = 100000, mid = 0;
-        while (left < right) {
-            mid = (left + right) / 2;
-            if (solvePuzzle(mid, diffs, times, limit))
-                right = mid;
-            else 
+        int answer = Integer.MAX_VALUE;
+        // 이분탐색으로 숙련도를 정하고
+        int left = 1;
+        int right = Arrays.stream(diffs).max().getAsInt();
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            // 각 숙련도마다 되는지 안되는지 판단하는 로직    
+            long time = 0;
+            int level = mid;
+            
+            for (int i = 0; i < diffs.length; i++) {
+                if (diffs[i] <= level) {
+                    time += times[i];
+                } else {
+                    int mul = diffs[i] - level;
+                    time += mul * (times[i] + times[i-1]) + times[i];
+                }
+                
+                if (time > limit) {
+                    break;
+                }
+            }
+            
+            if (time <= limit) {
+                answer = Math.min(answer, mid);
+                right = mid - 1;
+            } else {
                 left = mid + 1;
+            }
+            
         }
-        return (int) right;
-    }
-    
-    static boolean solvePuzzle(long level, int[] diffs, int[] times, long limit) {
-        long result = 0;
-        for (int i = 0; i < diffs.length; i++) {
-            if (level >= diffs[i]) 
-                result += times[i];
-            else 
-                result += (diffs[i] - level) * (times[i] + times[i-1]) + times[i];
-            if (result > limit) return false;
-        }
-        return true;
+        return answer;
     }
 }
